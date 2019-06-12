f07324ea5b00548b4c2ccbaf9d3e7a170c1e0530
yangguoqiang01 <wochange@163.com>
2019-06-12 22:54:02 +0800

    :construction: log日志的测试

--- src/main/java/com/faith/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/faith/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,7 @@ package com.faith.o2o.web.superadmin;
 
 import com.faith.o2o.entity.Area;
 import com.faith.o2o.service.AreaService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+// import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Auther: yangguoqiang01
@@ -23,6 +27,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    // 引入日志
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
 
     @Autowired
     private AreaService areaService;  // 创建Service实体类
@@ -30,6 +36,9 @@ public class AreaController {
     @RequestMapping(value = "/listareas", method = RequestMethod.GET)
     @ResponseBody  // 返回json
     private Map<String, Object> listArea() {
+        // 使用logger
+        logger.info("+++start++++");
+        long startTime = System.currentTimeMillis();
         // 存储返回值
         Map<String, Object> modelMap = new HashMap<String, Object>();
 
@@ -45,6 +54,10 @@ public class AreaController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        logger.error("测试错误");
+        long endTime = System.currentTimeMillis();
+        logger.debug("花费时间：[{}ms]", endTime - startTime);
+        logger.info("+++end++++");
         return modelMap;
     }
 }
