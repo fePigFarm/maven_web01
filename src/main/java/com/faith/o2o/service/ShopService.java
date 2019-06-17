ec2401f9f51fabb3be4b7ba7a7b9d6fcabe93ea0
yangguoqiang01 <wochange@163.com>
2019-06-17 23:57:31 +0800

    :construction: 增加service

---
+++ src/main/java/com/faith/o2o/service/ShopService.java
@@ -0,0 +1,16 @@
+package com.faith.o2o.service;
+
+import com.faith.o2o.dto.ShopExecution;
+import com.faith.o2o.entity.Shop;
+
+import java.io.File;
+
+/**
+ * @Auther: yangguoqiang01
+ * @Date: 2019-06-17
+ * @Description: com.faith.o2o.service
+ * @version: 1.0
+ */
+public interface ShopService {
+    ShopExecution addShop(Shop shop, File shopImg);
+}
---
+++ src/main/java/com/faith/o2o/service/impl/ShopServiceImpl.java
@@ -0,0 +1,20 @@
+package com.faith.o2o.service.impl;
+
+import com.faith.o2o.dto.ShopExecution;
+import com.faith.o2o.entity.Shop;
+import com.faith.o2o.service.ShopService;
+
+import java.io.File;
+
+/**
+ * @Auther: yangguoqiang01
+ * @Date: 2019-06-17
+ * @Description: com.faith.o2o.service.impl
+ * @version: 1.0
+ */
+public class ShopServiceImpl implements ShopService {
+
+    public ShopExecution addShop(Shop shop, File shopImg) {
+        return null;
+    }
+}
